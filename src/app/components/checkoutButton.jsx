3ee7d415ca9214b8860421297606298f1482a641
BigSzy <szymon.kaluza@outlook.com>
2024-01-06 12:15:41 +0000

    headers commenting in

--- src/app/components/checkoutButton.jsx
+++ src/app/components/checkoutButton.jsx
@@ -1,45 +1,39 @@
-import { loadStripe } from "@stripe/stripe-js";
-import { redirect } from "next/dist/server/api-utils";
 import { useBasket } from "./shoppingBasket";
 import React from "react";
-import styles from "../styles/checkoutbutton.module.scss"
+import styles from "../styles/checkoutbutton.module.scss";
 
 function CheckoutButton() {
-
-    const { items } = useBasket()
-
-
+  const { items } = useBasket();
 
   const redirectToCheckout = async () => {
     try {
-
-      const data = items.map(p => {return{
-        id: p.id,
-        qty: p.qty
-      }})
+      const data = items.map((p) => {
+        return {
+          id: p.id,
+          qty: p.qty,
+        };
+      });
 
       const checkoutResponse = await fetch("/api/checkout/", {
         method: "POST",
-        // headers: {
-        //   "Content-Type": "application/json",
-        // },
-        body:JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
       });
 
       const url = await checkoutResponse.json();
-      
-      window.open(url)
 
+      window.open(url);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <button 
-    className={styles.button}
-    onClick={() => redirectToCheckout()}>
-      CHECKOUT SERCURELY</button>
+    <button className={styles.button} onClick={() => redirectToCheckout()}>
+      CHECKOUT SERCURELY
+    </button>
   );
 }
 
